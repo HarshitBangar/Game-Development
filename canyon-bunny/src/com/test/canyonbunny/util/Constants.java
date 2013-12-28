af11a8347a63d1c98c581880b19e2f64167aab73
Harshit Bangar <hbangar@kiwiup.com>
2013-12-28 23:40:26 +0530

    Initial Commit

---
+++ canyon-bunny/src/com/test/canyonbunny/util/Constants.java
@@ -0,0 +1,15 @@
+package com.test.canyonbunny.util;
+
+//Class for Storing the configs
+
+public class Constants {
+	
+	public static final float VIEWPORT_WIDTH = 5.0f;
+	
+	public static final float VIEWPORT_HEIGHT =  5.0f;
+	
+	// Location of description file for texture atlas
+	public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack";
+
+
+}
